ac5d91b3160347ecaa2634683c54d98561c36938
Denning <gichabadenning254@gmail.com>
2024-07-13 19:44:31 +0300

    using find() to isolate a specific result

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// Example declarations in index.js
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+
+  function fuzzyMatch(drivers, partial) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(partial.toLowerCase()));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
